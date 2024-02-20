cc5080c6518d4264e0629bc8137b1fff57ea30c5
Prakhar Gupta <prakharyologupta@gmail.com>
2024-02-21 02:32:31 +0530

    Text-To-Speech

--- demo/src/main/java/com/captcha/demo/Audio/TextToSpeechService.java
+++ demo/src/main/java/com/captcha/demo/Audio/TextToSpeechService.java
@@ -1,14 +1,12 @@
 package com.captcha.demo.Audio;
 
+
 import java.io.ByteArrayOutputStream;
 import java.util.Locale;
-
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-
 import org.springframework.stereotype.Service;
-
 import marytts.LocalMaryInterface;
 import java.util.Base64;
 
@@ -21,22 +19,20 @@ public class TextToSpeechService {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		LocalMaryInterface mary = null;
 		mary = new LocalMaryInterface();
-		
 		AudioInputStream audio = null;
-		
 		mary.setLocale(Locale.US);
 		mary.setVoice("cmu-rms-hsmm");
 		audio = mary.generateAudio(text);
 		AudioSystem.write(audio, AudioFileFormat.Type.WAVE, outputStream);
 		byte[] audioBytes = outputStream.toByteArray(); 
-        return Base64.getEncoder().encodeToString(audioBytes);
+        String baseString = Base64.getEncoder().encodeToString(audioBytes);
+        return baseString;
     }
     catch (Exception e) {
         e.printStackTrace();
         return null;
 	}
 }
-
     public static void main(String[] args) {
         TextToSpeechService textToSpeechService = new TextToSpeechService();
         try {
